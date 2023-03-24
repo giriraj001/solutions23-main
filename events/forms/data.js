13eb41b445fb23feaea0d7992f0cac5a4d3edd61
giriraj001 <94731482+giriraj001@users.noreply.github.com>
2023-03-25 04:40:20 +0530

    forms logic modified

--- events/forms/data.js
+++ events/forms/data.js
@@ -2,93 +2,93 @@ export var google_form_links = [
   // coding category id=0
   [
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true" width="640" height="6259" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true" width="640" height="6259" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
   ],
 
   // robotics category id=1
   [
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
   ],
 
   // open category id=2
   [
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
   ],
 
   // machine learning category id=3
   [
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
   ],
 
   // game dev & cyber sec. category id=4
   [
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
   ],
 
   // gaming category id=5
   [
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
     [
-      '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+      '<iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdwFcOaSgaKyqOcf1UZXdf4C38vsuNjVifJHAaJMExPG-TsTA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
     ],
   ],
 ];
