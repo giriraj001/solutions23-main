c0f2cfad3cc8e6e6d11a2701f2db7b4809977dd1
giriraj001 <[girirajsingh01042002@gmail.com]>
2023-04-11 00:05:17 +0530

    robotics link added

--- schedule/data.js
+++ schedule/data.js
@@ -1,18 +1,19 @@
 let schedule_data = [
   [
-    { name: "TBA", time: "(TBA)" },
-    { name: "TBA", time: "(TBA)" },
-    { name: "TBA", time: "(TBA)" },
+    { name: "Techmatrix", time: "3:30PM to 4:00Pm" },
+    { name: "Placement Apti", time: "4:30PM to 5:00Pm" },
+    { name: "She Code", time: "6:30PM to 8:00Pm" },
+    { name: "Imaginate", time: "11:30AM" },
+  ],
+  [
+    { name: "Shortest Code", time: "1:30PM to 3:30PM" },
+    { name: "Photography", time: "11:50PM" },
+    { name: "Arcade", time: "11:50PM" },
+    { name: "Flash 500", time: "11:50PM" },
     { name: "TBA", time: "(TBA)" },
   ],
   [
-    { name: "TBA", time: "(TBA)" },
-    { name: "TBA", time: "(TBA)" },
-    { name: "TBA", time: "(TBA)" },
-    { name: "TBA", time: "(TBA)" },
-  ],
-  [
-    { name: "TBA", time: "(TBA)" },
+    { name: "How Unicorn", time: "3:00PM" },
     { name: "TBA", time: "(TBA)" },
     { name: "TBA", time: "(TBA)" },
     { name: "TBA", time: "(TBA)" },
